72ce326fe452bca6fc024792dde4b7b2d28f4856
AcHRaF AiSsaOui <achr3012@gmail.com>
2023-11-30 15:55:57 +0100

    Update next v

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
   },
 };
 
